523903ff934759cca8e36820cd6e9f77d5f5e699
Kenneth Ortiz <kenortiz@users.noreply.github.com>
2016-04-06 03:21:28 +0000

    Installed carrierwave, figaro and fog gems. Generated uploader called Picture. Generated migration to add picture column to grams table. Added picture field to grams form. Added picture to grams params. Changed storage to fog.

--- Gemfile
+++ Gemfile
@@ -46,6 +46,11 @@ gem 'simple_form'
 
 gem 'devise'
 
+gem 'carrierwave'
+
+gem 'figaro'
+
+gem 'fog', '~> 1.3.1'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,5 +1,6 @@
 class Gram < ActiveRecord::Base
   validates :message, presence: true, length: { minimum: 3, maximum: 50 }
+  mount_uploader :picture, PictureUploader
 
   belongs_to :user
 end
