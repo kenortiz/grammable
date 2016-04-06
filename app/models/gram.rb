ad6e81c7a9ed9272bff8ae15b69dea9e618f44b6
Kenneth Ortiz <kenortiz@users.noreply.github.com>
2016-04-06 04:49:10 +0000

    Added validation for picture in gram model. Adjusted rails_helper.rb to include ActionDispatch::TestProcess. Added default picture to gram factory. Edited gram post test to include default picture.

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,5 +1,7 @@
 class Gram < ActiveRecord::Base
   validates :message, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :picture, presence: true
+  
   mount_uploader :picture, PictureUploader
 
   belongs_to :user
