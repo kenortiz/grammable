272aa1395cb755804c0e13ff342cc35dfee6cf9a
Kenneth Ortiz <kenortiz@users.noreply.github.com>
2016-04-11 01:03:53 +0000

    Added comment model and controller. Added tests for comments. Nested create resource for comments under gram. Added create action in comments controller and updated gram, user and comment models.

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,7 +1,8 @@
 class Gram < ActiveRecord::Base
   validates :message, presence: true, length: { minimum: 3, maximum: 50 }
   validates :picture, presence: true
-  
+  has_many :comments
+
   mount_uploader :picture, PictureUploader
 
   belongs_to :user
